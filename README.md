240f7e59dd55cd56f13f79065d336b1f90f64b03
stanlyshi <85752240+stanlyshi@users.noreply.github.com>
2021-06-11 23:08:13 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,84 @@
+![license](https://img.shields.io/github/license/roacn/Actions-OpenWrt-Lede-N1?color=ff69b4)
+![N1_plus](https://github.com/roacn/Actions-OpenWrt-Lede-N1/actions/workflows/single_plus.yml/badge.svg)
+![N1_mini](https://github.com/roacn/Actions-OpenWrt-Lede-N1/actions/workflows/single_mini.yml/badge.svg)
+![Build lede](https://github.com/roacn/Actions-OpenWrt-Lede-N1/actions/workflows/N1_Multi.yml/badge.svg)
+![code-size](https://img.shields.io/github/languages/code-size/roacn/Actions-OpenWrt-Lede-N1?color=blueviolet)
+
+
+ ![applist](https://github.com/roacn/N1Openwrt/blob/master/imgs/N1-OpenWrt.jpg?raw=true)
+
+# 源码来源：
+
+[![Lean](https://img.shields.io/badge/source-Lean-red.svg?style=flat&logo=appveyor)](https://github.com/coolsnowwolf/lede) 
+[![ophub](https://img.shields.io/badge/kernel-ophub-orange.svg?style=flat&logo=appveyor)](https://github.com/ophub/amlogic-s9xxx-openwrt) 
+[![unifreq](https://img.shields.io/badge/package-unifreq-yellow.svg?style=flat&logo=appveyor)](https://github.com/unifreq/openwrt_packit) 
+[![P3TERX](https://img.shields.io/badge/actions-P3TERX-success.svg?style=flat&logo=appveyor)](https://github.com/P3TERX/Actions-OpenWrt)
+[![mingxiaoyu](https://img.shields.io/badge/actions-mingxiaoyu-blue.svg?style=flat&logo=appveyor)](https://github.com/mingxiaoyu)
+[![helloworld](https://img.shields.io/badge/ssr-fw876-blueviolet.svg?style=flat&logo=appveyor)](https://github.com/fw876/helloworld)
+[![apps](https://img.shields.io/badge/app-kenzok8-violet.svg?style=flat&logo=appveyor)](https://github.com/kenzok8)
+
+
+
+# 如何使用
+1. fork项目
+2. 在secrets中创建RELEASES_TOKEN，一般一次编译要2~4小时，所以要创建一个github发布用的token。
+3. 点击Actions -> Workflows -> Run workflow -> Run workflow 
+4. N1 Multiple Version 多版本编译
+5. N1 Single Version 只编译一个版本
+6. Update Checker, 上游更新则编译。MULTIPLE_BUILD: true **编译mini和plus多版本编译**  false，**只编译mini**
+
+------
+
+## 用户名和密码
+ * User: root
+ * Password: password
+ * Default IP: 192.168.1.2
+------
+## 防火墙
+默认自定义防火墙: iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+
+## app list:（mini 图不代表最新的）
+ * jd-dailybonus
+ * passwall
+ * ssr+
+ * ServerChan
+ * aria2
+ * ddns
+ * samba4
+ * KMS 
+ * docker
+ * zerotier
+ * flowoffload
+ 
+ ![applist](https://github.com/mingxiaoyu/N1Openwrt/blob/master/imgs/mini.jpg?raw=true)
+ 
+ ------
+ # N1 U盘写入刷emmc
+```
+cd      /root
+./install-to-emmc.sh
+```
+如果一直卡在fdisk失败那里的解决办法：一是再多试几次，如果还不成功，则需要手动清空分区表然后再重试，具体命令:
+```
+  dd   if=/dev/zero   of=/dev/mmcblk2  bs=512  count=1  &&  sync
+```
+
+升级降级方法统一为：
+把 update-amlogic-openwrt.sh 及 img镜像上传至  /mnt/mmcblk2p4
+```
+cd    /mnt/mmcblk2p4
+chmod   755  update-amlogic-openwrt.sh
+./update-amlogic-openwrt.sh    xxxxx.img
+```
+
+# 固件发布:
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/roacn/Actions-OpenWrt-Lede-N1?style=for-the-badge&label=下载&&color=00aa66)](https://github.com/roacn/Actions-OpenWrt-Lede-N1/releases/latest)
+------
+ # 感激 
+  * [mingxiaoyu](https://github.com/mingxiaoyu)
+  * [P3TERX](https://github.com/P3TERX/Actions-OpenWrt)提供的脚本参考
+ 
+ ## 云编译的规格
+https://docs.github.com/en/actions/reference/virtual-environments-for-github-hosted-runners#supported-runners-and-hardware-resources
+
---
+++ diy.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# 修改N1做旁路由的防火墙设置
+#If the interface is eth0.
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+#If the interface is br-lan bridged.
+#echo "iptables -t nat -I POSTROUTING -o br-lan -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
+pushd package/lean
+
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic
+# luci-lib-fs为依赖库
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-lib-fs
+
+# Add luci-app-cpufreq
+rm -rf ./luci-app-cpufreq
+# svn co https://github.com/roacn/luci-app-cpufreq/trunk/luci-app-cpufreq
+git clone https://github.com/roacn/luci-app-cpufreq
+
+#Add luci-app-jd-dailybonus
+#rm -rf ./luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
+
+# Add luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld
+# cat > helloworld/luci-app-ssr-plus/root/etc/ssrplus/black.list << EOF
+# services.googleapis.cn
+# googleapis.cn
+# heroku.com
+# githubusercontent.com 
+# EOF
+popd
+
+# Clone community packages to package/community
+mkdir package/community
+
+pushd package/community
+
+# Add luci-app-adguardhome
+# depth用于指定克隆深度，为1即表示只克隆最近一次commit.
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+
+
+# Add luci-app-diskman
+git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+mkdir parted
+cp luci-app-diskman/Parted.Makefile parted/Makefile
+
+# Add luci-app-dockerman
+# git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+# git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+# svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker
+# svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+
+# Add luci-app-openclash
+# git clone --depth=1 https://github.com/vernesong/OpenClash
+
+# Add luci-app-serverchan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add luci-app-onliner (need luci-app-nlbwmon)
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+rm -rf ./openwrt-packages/luci-app-serverchan
+rm -rf ./openwrt-packages/luci-app-ssr-plus
+rm -rf ./openwrt-packages/luci-theme-argon_new
+rm -rf ./openwrt-packages/luci-app-jd-dailybonus
+
+git clone --depth=1 https://github.com/kenzok8/small
+rm -rf ./small/v2ray-plugin
+rm -rf ./small/xray-core
+rm -rf ./small/xray-plugin
+rm -rf ./small/shadowsocks-rust
+
+popd
+
+
+# Add luci-app-dockerman
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+if [ -e feeds/packages/utils/docker-ce ];then
+	sed -i '/dockerd/d' package/luci-app-dockerman/Makefile
+	sed -i 's/+docker/+docker-ce/g' package/luci-app-dockerman/Makefile
+fi
+
+# Mod zzz-default-settings
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
+
+# Openwrt version
+#version=$(grep "DISTRIB_REVISION=" package/lean/default-settings/files/zzz-default-settings  | awk -F "'" '{print $2}')
+#sed -i '/DISTRIB_REVISION/d' package/lean/default-settings/files/zzz-default-settings
+#echo "echo \"DISTRIB_REVISION='${version} $(TZ=UTC-8 date "+%Y.%m.%d") '\" >> /etc/openwrt_release" >> package/lean/default-settings/files/zzz-default-settings
+#sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
+#echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
